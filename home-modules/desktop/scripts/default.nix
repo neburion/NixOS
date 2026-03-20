babebeb61f0f039765d1f27f780786551156e6e4
neburion <neburion@proton.me>
2026-03-20 02:10:23 -0400

    fix the script path in the imports

--- home-modules/desktop/scripts/default.nix
+++ home-modules/desktop/scripts/default.nix
@@ -1,8 +1,8 @@
 { pkgs, ... }:
 
 let
-  wofi-power-menu    = import ./scripts/wofi-power-menu.nix    { inherit pkgs; };
-  wofi-theme-switcher = import ./scripts/wofi-theme-switcher.nix { inherit pkgs; };
+  wofi-power-menu     = import ./wofi-power-menu.nix     { inherit pkgs; };
+  wofi-theme-switcher = import ./wofi-theme-switcher.nix { inherit pkgs; };
 in
 {
   home.packages = [
