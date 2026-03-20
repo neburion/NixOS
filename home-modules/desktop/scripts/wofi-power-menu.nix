6245e42b90adca5408238244be2cbdc1b9e75e0f
neburion <neburion@proton.me>
2026-03-20 02:13:05 -0400

    working on the script look

--- home-modules/desktop/scripts/wofi-power-menu.nix
+++ home-modules/desktop/scripts/wofi-power-menu.nix
@@ -1,16 +1,29 @@
 { pkgs, ... }:
 
 pkgs.writeShellScriptBin "wofi-power-menu" ''
-  shutdown="Shutdown"
-  reboot="Reboot"
-  suspend="Suspend"
-  lock="Lock"
-  logout="Logout"
+  shutdown="  Shutdown"
+  reboot="  Reboot"
+  suspend="  Suspend"
+  lock="  Lock"
+  logout="  Logout"
 
   chosen=$(printf "%s\n%s\n%s\n%s\n%s" \
     "$shutdown" "$reboot" "$suspend" "$lock" "$logout" \
-    | wofi --show dmenu --width 300 --height 250 \
-           --prompt "Power:" --cache-file /dev/null)
+    | wofi --show dmenu \
+           --prompt ">" \
+           --width "35%" \
+           --height "30%" \
+           --location center \
+           --orientation vertical \
+           --columns 1 \
+           --normal_window \
+           --layer overlay \
+           --hide_scroll \
+           --no_actions \
+           --gtk_dark \
+           --insensitive=false \
+           --hide_search=false \
+           --cache-file /dev/null)
 
   case "$chosen" in
     "$shutdown") systemctl poweroff ;;
