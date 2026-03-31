e737d6a8f4ad313f04ada119d1f8ce0ce45b0eee
neburion <neburion@proton.me>
2026-03-31 15:00:10 -0400

    removing buggy envirement variables from hyprland/env.nix

--- home-modules/desktop/wm/hyprland/env.nix
+++ home-modules/desktop/wm/hyprland/env.nix
@@ -17,10 +17,8 @@
       # Nvidia
       "XDG_SESSION_TYPE,wayland"
       "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-      #"__NV_PRIME_RENDER_OFFLOAD,1"
       "__GL_GSYNC_ALLOWED,1"
       "__GL_VRR_ALLOWED,0"
-      #"__VK_LAYER_NV_optimus,NVIDIA_only"
       "NVD_BACKEND,direct"
       "GDK_BACKEND,wayland,x11,*"
       "QT_QPA_PLATFORM,wayland;xcb"
