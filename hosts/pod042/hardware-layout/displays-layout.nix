b98db127637a3c414b8a725f512b1023e6eb38df
neburion <neburion@proton.me>
2026-07-01 17:19:20 -0400

    fixing displays-layout.nix.

--- hosts/pod042/hardware-layout/displays-layout.nix
+++ hosts/pod042/hardware-layout/displays-layout.nix
@@ -1,5 +1,12 @@
+{ lib, ... }:
+
 {
-  host.displays = {
+  options.displays = {
+    primary = lib.mkOption { type = lib.types.attrs; };
+    monitors = lib.mkOption { type = lib.types.attrs; };
+  };
+
+  config.displays = {
     primary = {
       width  = 1920;
       height = 1080;
@@ -10,11 +17,13 @@
         name     = "eDP-1";
         mode     = "1920x1080@120";
         position = "0x0";
+        scale    = "1";
       };
       external = {
         name     = "HDMI-A-1";
         mode     = "1920x1080@60";
         position = "1920x0";
+        scale    = "1";
       };
     };
   };
