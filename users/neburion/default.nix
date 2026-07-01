d1ddd0c6edfede672aa52e29e5a57811710192c9
neburion <neburion@proton.me>
2026-07-01 17:26:17 -0400

    fixing sperfile/superfile typo.

--- users/neburion/default.nix
+++ users/neburion/default.nix
@@ -9,7 +9,7 @@
     ../../modules/home/base.nix
     ../../modules/home/cli/shell/fish.nix
     ../../modules/home/cli/neovim
-    ../../modules/home/cli/sperfile.nix
+    ../../modules/home/cli/superfile.nix
     ../../modules/home/cli/compression.nix
     ../../modules/home/cli/fastfetch.nix
     ../../modules/home/cli/btop.nix
