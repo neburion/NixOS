243f481e45c24bc0048da0ea2a8255eda59e259e
neburion <neburion@proton.me>
2026-07-12 11:28:55 -0400

    adding fish to printer-server

--- users/printer/default.nix
+++ users/printer/default.nix
@@ -3,6 +3,7 @@
 {
   home-manager.users.printer.imports = [
     ../../modules/home/base.nix
+    ../../modules/home/cli/shell/fish.nix
     ../../modules/home/cli/btop.nix
     ../../modules/home/cli/compression.nix
     ../../modules/home/cli/fastfetch.nix
